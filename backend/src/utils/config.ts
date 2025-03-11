62621c740d2d80dcd88b6c039fc66f456f7109e5
darrenkuro <odon5ht@gmail.com>
2025-03-11 23:24:28 +0100

    Fix typos

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -12,8 +12,8 @@ const configSchema = z.object({
             invalid_type_error: "must be a number",
         })
         .int("must be an integer")
-        .min(1024, "must be greater than 1024") // Avoid privileged / reversed
-        .max(65535, "must be less than 65535"),
+        .min(1024, "must be greater than 1023") // Avoid privileged / reversed
+        .max(65535, "must be less than 65536"),
 
     JWT_SECRET: z.string().min(32, "must be at least 32 characters long"),
     DB_PATH: z.string().min(1, "cannot be empty"),
