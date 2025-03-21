2519b65cabcd478d60044cd8eea87968678242a3
darrenkuro <odon5ht@gmail.com>
2025-03-21 06:39:15 +0100

    Add swagger UI for API end points

--- backend/src/utils/app.ts
+++ backend/src/utils/app.ts
@@ -1,6 +1,13 @@
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
-import fastify, { FastifyInstance, FastifyServerOptions } from "fastify";
+import fastify, {
+    FastifyInstance,
+    FastifyReply,
+    FastifyRequest,
+    FastifyServerOptions,
+} from "fastify";
+import configPlugin from "./config.ts";
+import swaggerPlugin from "./swagger.ts";
 import dbPlugin from "../db/db.plugin.ts";
 import wsPlugin from "../ws/ws.plugin.ts";
 import authPlugin from "../auth/auth.plugin.ts";
@@ -8,7 +15,6 @@ import userPlugin from "../user/user.plugin.ts";
 import gamePlugin from "../game/game.plugin.ts";
 import tournamentPlugin from "../tournament/tournament.plugin.ts";
 import { seed } from "../db/db.seed.ts";
-import configPlugin from "./config.ts";
 
 export const buildApp = async (
     opts: FastifyServerOptions,
@@ -28,12 +34,18 @@ export const buildApp = async (
         // TODO: Add more options here for JWT, see: https://github.com/fastify/fastify-jwt
         await app.register(jwt, { secret: app.config.jwtSecret }); // Register jwt plugin
 
+        await app.register(swaggerPlugin); // Register swagger plugin
+
         await app.register(dbPlugin); // Register database plugin
         await app.register(wsPlugin); // Register websocket plugin
         await app.register(authPlugin); // Register auth plugin
         await app.register(userPlugin); // Register user plugin
         await app.register(gamePlugin); // Register game plugin
         await app.register(tournamentPlugin); // Register tournament plugin
+
+        app.get("/healthcheck", async (_: FastifyRequest, reply: FastifyReply) => {
+            return reply.status(200).send({ status: "ok" });
+        });
 
         if (!test) {
             /** Seed database if not in production */
