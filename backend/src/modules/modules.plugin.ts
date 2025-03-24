3e28314f8feae185d48677ed0544dd12811297cc
darrenkuro <odon5ht@gmail.com>
2025-03-24 20:30:10 +0100

    Fix import order, unify styles for modules plugin

--- backend/src/modules/modules.plugin.ts
+++ backend/src/modules/modules.plugin.ts
@@ -1,16 +1,17 @@
+import type { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
 import authPlugin from "./auth/auth.plugin.ts";
-import userPlugin from "./user/user.plugin.ts";
+// import friendPlugin from "./friend/friend.plugin";
 import gamePlugin from "./game/game.plugin.ts";
 import tournamentPlugin from "./tournament/tournament.plugin.ts";
-// import friendPlugin from "./friend/friend.plugin";
-import type { FastifyInstance } from "fastify";
+import userPlugin from "./user/user.plugin.ts";
 
-const modulesPlugin = async (fastify: FastifyInstance) => {
-    await fastify.register(authPlugin);
-    await fastify.register(userPlugin);
-    await fastify.register(gamePlugin);
-    await fastify.register(tournamentPlugin);
+const modulesPlugin = async (app: FastifyInstance) => {
+    await app.register(authPlugin);
+    // await app.register(friendPlugin);
+    await app.register(gamePlugin);
+    await app.register(tournamentPlugin);
+    await app.register(userPlugin);
 };
 
 export default fp(modulesPlugin, {
