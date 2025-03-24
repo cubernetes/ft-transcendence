467ac614d6e26e7a16802e7efe17b79ccbf4df19
darrenkuro <odon5ht@gmail.com>
2025-03-24 01:24:11 +0100

    Add type safefy for auth module

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,20 +1,29 @@
 import bcrypt from "bcrypt";
 import type { FastifyInstance } from "fastify";
 import { User } from "../user/user.types.ts";
+import { JwtPayload } from "./auth.types.ts";
 
 export const createAuthService = (fastify: FastifyInstance) => {
-    const jwt = fastify.jwt;
+    const { jwt } = fastify;
+    const saltRounds = 10;
 
-    const hashPassword = (password: string) => bcrypt.hash(password, 10); // Salt rounds?
+    const hashPassword = (password: string): Promise<string> => bcrypt.hash(password, saltRounds);
 
-    const comparePassword = (password: string, hash: string) => bcrypt.compare(password, hash);
+    const comparePassword = (password: string, hash: string): Promise<boolean> =>
+        bcrypt.compare(password, hash);
 
-    const generateToken = (user: User, expiration: string = "1d") => {
+    const generateToken = (user: User, exp: string = "1d"): string => {
         const { id, username, displayName } = user;
-        return jwt.sign({ id, username, displayName }, { expiresIn: expiration });
+
+        const payload: JwtPayload = { id: String(id), username, displayName } satisfies Omit<
+            JwtPayload,
+            "iat" | "exp"
+        >;
+
+        return jwt.sign(payload, { expiresIn: exp });
     };
 
-    const verifyToken = (token: string) => jwt.verify(token);
+    const verifyToken = (token: string): JwtPayload => jwt.verify(token) as JwtPayload;
 
     return {
         hashPassword,
