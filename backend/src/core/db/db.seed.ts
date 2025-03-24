e75ca896719efa2b9eaca160e428745c1edaf409
darrenkuro <odon5ht@gmail.com>
2025-03-25 00:36:10 +0100

    Add guard to seed and remove try-catch block

--- backend/src/core/db/db.seed.ts
+++ backend/src/core/db/db.seed.ts
@@ -1,35 +1,38 @@
-//import { users } from "./schema";
+import type { FastifyInstance } from "fastify";
 import { faker } from "@faker-js/faker";
-import { FastifyInstance } from "fastify";
 
-const seedUsers = async (n: number, fastify: FastifyInstance) => {
+const seedUsers = async (n: number, app: FastifyInstance) => {
     const fakeUsers = Array.from({ length: n }, () => ({
         username: faker.internet.username(),
         displayName: faker.person.firstName(),
         passwordHash: faker.internet.password(),
     }));
 
-    fastify.log.info(`Seeding ${n} faker users...`);
+    app.log.info(`Seeding ${n} faker users...`);
 
     const seedOneUser = async (newUser: (typeof fakeUsers)[number]) => {
-        try {
-            const user = await fastify.userService.create(newUser);
+        const user = await app.userService.create(newUser);
 
-            const stats = {
-                wins: faker.number.int({ min: 0, max: 100 }),
-                losses: faker.number.int({ min: 0, max: 100 }),
-            };
-
-            await fastify.userService.update(user.id, stats);
-        } catch (err) {
-            fastify.log.error({ err }, "Failed to create or update user");
+        if (!user.success) {
+            return app.log.error({ err: user.error }, "Failed to create user");
         }
+
+        const stats = {
+            wins: faker.number.int({ min: 0, max: 100 }),
+            losses: faker.number.int({ min: 0, max: 100 }),
+        };
+
+        await app.userService.update(user.data.id, stats);
     };
 
     await Promise.all(fakeUsers.map(seedOneUser));
 };
 
-export const seed = async (fastify: FastifyInstance) => {
-    fastify.log.info(`Seed database with initial data...`);
-    await seedUsers(20, fastify);
+export const seed = async (app: FastifyInstance) => {
+    const userCount = await app.userService.getCount();
+
+    // Only seed users if there aren't a significant amount
+    if (userCount <= 200) {
+        await seedUsers(20, app);
+    }
 };
