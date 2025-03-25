403cb1f9e3035386d1214a0445b8d6fd9876657b
darrenkuro <odon5ht@gmail.com>
2025-03-25 03:47:05 +0100

    Change to verbose error message for zod

--- backend/src/core/config/config.schema.ts
+++ backend/src/core/config/config.schema.ts
@@ -3,13 +3,14 @@ import { z } from "zod";
 export const configSchema = z.object({
     BACKEND_PORT: z.coerce
         .number({
-            required_error: "cannot be empty",
-            invalid_type_error: "must be a number",
+            invalid_type_error: "BACKEND_PORT is required", // Coerce will force to nan
         })
-        .int("must be an integer")
-        .min(1024, "must be greater than 1023") // Avoid privileged or reversed ports
-        .max(65535, "must be less than 65536"),
+        .int("BACKEND_PORT must be an integer")
+        .min(1024, "BACKEND_PORT must be greater than 1023") // Avoid privileged or reversed ports
+        .max(65535, "BACKEND_PORT must be less than 65536"),
 
-    JWT_SECRET: z.string().min(32, "must be at least 32 characters long"),
-    DB_PATH: z.string().min(1, "cannot be empty"),
+    JWT_SECRET: z
+        .string({ required_error: "JWT_SECRET is required" })
+        .min(32, "JWT_SECRET must be at least 32 characters long"),
+    DB_PATH: z.string({ required_error: "DB_PATH is required" }).min(1, "DB_PATH cannot be empty"),
 });
