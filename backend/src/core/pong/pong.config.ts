f9710723c961dacc25426eabf55e9260a29e87b3
darrenkuro <odon5ht@gmail.com>
2025-03-27 10:48:13 +0100

    Clean up types and data structures for Pong engine

--- backend/src/core/pong/pong.config.ts
+++ backend/src/core/pong/pong.config.ts
@@ -1,14 +1,28 @@
 import { PongConfig } from "./pong.types.ts";
 
+const defaultBoard = { width: 20, height: 0.1, depth: 15 };
+const defaultBall = {
+    pos: { x: 0, y: 0, z: 0 },
+    vec: { x: 0.2, y: 0, z: 0.2 },
+    r: 0.3,
+    speed: 0.2,
+};
+
+const defaultPaddleLeft = {
+    pos: { x: -defaultBoard.width / 2 + 0.5, y: 0.5, z: 0 },
+    size: { width: 0.2, height: 0.3, depth: 5 },
+    speed: 0.3,
+};
+
+const defaultPaddleRight = {
+    pos: { x: defaultBoard.width / 2 - 0.5, y: 0.5, z: 0 },
+    size: { width: 0.2, height: 0.3, depth: 5 },
+    speed: 0.3,
+};
+
 export const defaultGameConfig: PongConfig = {
-    boardWidth: 20,
-    boardHeight: 0.1,
-    boardDepth: 15,
-    paddleWidth: 0.2,
-    paddleHeight: 0.3,
-    paddleDepth: 5,
-    ballRadius: 0.3,
-    paddleSpeed: 0.3,
-    ballSpeed: 0.2,
+    board: defaultBoard,
+    paddles: [defaultPaddleLeft, defaultPaddleRight],
+    ball: defaultBall,
     playTo: 11,
 };
