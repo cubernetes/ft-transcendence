6b1e851b473232653f45ab2f16f2be36068469f3
darrenkuro <odon5ht@gmail.com>
2025-03-29 17:59:00 +0100

    Employ stricter typecheck across all builds for backend

--- backend/src/modules/game/game.routes.ts
+++ backend/src/modules/game/game.routes.ts
@@ -6,7 +6,7 @@ import {} from // createGameHandler,
 "./game.controller.ts";
 // import { withZod } from "../../utils/zod-validate.ts";
 
-const gameRoutes: FastifyPluginAsync = async (app) => {
+const gameRoutes: FastifyPluginAsync = async (_) => {
     //app.post("/create", withZod({ body: createGameSchema }, createGameHandler));
     //app.get("/id/:id", withZod({ params: gameIdSchema }, getGameByIdHandler));
     //app.get("/all", getAllGamesHandler);
