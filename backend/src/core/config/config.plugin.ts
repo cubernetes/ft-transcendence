63feb8abdbff37de1fc863ae8925e98d26291086
darrenkuro <odon5ht@gmail.com>
2025-03-29 17:49:49 +0100

    Refactor vault reading to comply with test env

--- backend/src/core/config/config.plugin.ts
+++ backend/src/core/config/config.plugin.ts
@@ -5,53 +5,24 @@ import fs from "fs";
 import { configSchema } from "./config.schema.ts";
 import { AppConfig } from "./config.types.ts";
 
-const readVaultOnce = async (path: string) => {
-	const vaultToken = fs.readFileSync("/run/secrets/backend_vault_token", "utf8");
-
-	const promise = fetch(`http://vault:8200/v1/${path}`, {headers:{"X-Vault-Token": vaultToken}})
-		.then((resp) => {
-			if (!resp.ok) {
-				throw new Error(`Request to vault API was NOT OK:\n${resp.text()}`);
-			}
-			return resp.text();
-		})
-		.then((text) => {
-			try {
-				return JSON.parse(text).data.data;
-			} catch (e) {
-				throw new Error(`Failed to parse JSON response:\n${text}`);
-			}
-		})
-		.catch((e) => {
-			throw new Error(`Request to vault API FAILED:\n${e}`);
-		});
-
-	fs.writeFileSync("/run/secrets/backend_vault_token", ""); // clear, since not needed anymore, but token-max-use should be set to 1 anyways
-
-	return promise;
-}
-
 /** NODE_ENV should be used as process.env.NODE_ENV to ensure dead code is removed by esbuild */
 const configPlugin = async (app: FastifyInstance): Promise<void> => {
     // Validate config integrity with zod schema
-
-
-	const secrets = await readVaultOnce("secret/data/backend");
-	const rawJwtSecret = secrets.JWT_SECRET;
-
-    const parsedEnv = configSchema.parse({...process.env, JWT_SECRET: rawJwtSecret});
+    const parsedEnv = configSchema.parse(process.env);
 
     const port = parsedEnv.BACKEND_PORT;
     const jwtSecret = parsedEnv.JWT_SECRET;
     const dbPath = parsedEnv.DB_PATH;
+    const apiPrefix = parsedEnv.API_PREFIX;
+    const host = parsedEnv.HOST;
+    const domains = parsedEnv.DOMAINS;
+
+    // DB_DIR can be explicitly set, for testing purposes when DB_PATH is ":memory:"
     const dbDir = process.env.DB_DIR ?? path.dirname(parsedEnv.DB_PATH);
     if (!fs.existsSync(dbDir)) {
         throw new Error("Directory for database does not exist");
     }
 
-    const apiPrefix = parsedEnv.API_PREFIX;
-    const host = parsedEnv.HOST;
-    const domains = parsedEnv.DOMAINS;
     const corsOrigin =
         process.env.NODE_ENV === "production"
             ? domains.flatMap((d) => [`https://${d}`, `http://${d}`])
