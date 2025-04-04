b4a640396405049191b748cb3e6bb0e8f174fbd9
tsi <timo42@proton.me>
2025-04-04 14:13:06 +0200

    Add separate 2FA setup page in home page (and make code more flexible)

--- frontend/src/scripts/components/components.header.ts
+++ frontend/src/scripts/components/components.header.ts
@@ -14,19 +14,26 @@ export const createHeader = async (): Promise<HTMLElement> => {
     const nav = document.createElement("nav");
     nav.className = "flex items-center space-x-6";
 
+    const navItems = {
+        Home: "home",
+        Game: "game",
+        Leaderboard: "leaderboard",
+        Profile: "profile",
+        Login: "login",
+        "2FA Setup": "totpSetup",
+    };
     const navList = document.createElement("ul");
     navList.className = "flex text-1xl space-x-4";
 
-    const navItems = ["Setup", "Game", "Leaderboard", "Profile", "Simulation"];
-    navItems.forEach((item) => {
+    for (const [title, hashId] of Object.entries(navItems)) {
         const li = document.createElement("li");
         const link = document.createElement("a");
-        link.href = `#${item.toLowerCase()}`;
-        link.textContent = item;
+        link.href = `#${hashId}`;
+        link.textContent = title;
         link.className = "hover:underline";
         li.appendChild(link);
         navList.appendChild(li);
-    });
+    }
 
     const loginStatus = document.createElement("li");
     navList.appendChild(loginStatus);
