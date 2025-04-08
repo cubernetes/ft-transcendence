c1be8c43493fca6b125faf85f02f40180a8aeda2
darrenkuro <odon5ht@gmail.com>
2025-04-08 22:29:36 +0200

    Comment out code to be refactored so the app builds

--- frontend/src/scripts/ui/pages/RemoteGamePage.ts
+++ frontend/src/scripts/ui/pages/RemoteGamePage.ts
@@ -1,53 +1,53 @@
-import { createPongEngine } from "@darrenkuro/pong-core";
-import { createEl } from "../../utils/dom-helper";
-import { createFooter } from "../layout/Footer";
-import { createHeader } from "../layout/Header";
-import { GameInstance } from "./game/renderer/game.instance";
+// import { createPongEngine } from "@darrenkuro/pong-core";
+// import { createEl } from "../../utils/dom-helper";
+// import { createFooter } from "../layout/Footer";
+// import { createHeader } from "../layout/Header";
+// import { GameInstance } from "./game/renderer/game.instance";
 
-/**
- * Remote vs local vs "ai".
- */
-export const createRemoteGamePage = async (
-    mode: "remote" | "local" | "ai"
-): Promise<HTMLElement[]> => {
-    const header = await createHeader();
-    const footer = createFooter();
+// /**
+//  * Remote vs local vs "ai".
+//  */
+// export const createRemoteGamePage = async (
+//     mode: "remote" | "local" | "ai"
+// ): Promise<HTMLElement[]> => {
+//     const header = await createHeader();
+//     const footer = createFooter();
 
-    const canvas = createEl("canvas", "w-full h-full", { attributes: { id: "renderCanvas" } });
-    const container = createEl("div", "w-full h-[600px] relative", { children: [canvas] });
+//     const canvas = createEl("canvas", "w-full h-full", { attributes: { id: "renderCanvas" } });
+//     const container = createEl("div", "w-full h-[600px] relative", { children: [canvas] });
 
-    const gameInstance = await GameInstance.getInstance(canvas);
-    if (!gameInstance) {
-        window.log.error("Couldn't find game instance");
-    }
+//     const gameInstance = await GameInstance.getInstance(canvas);
+//     if (!gameInstance) {
+//         window.log.error("Couldn't find game instance");
+//     }
 
-    if (mode === "remote") {
-        // socket, send game start
-        document.addEventListener("keydown", (event) => {
-            window.log.info(`Key pressed: ${event.key}`);
-            if (event.key === "ArrowUp" || event.key === "w") {
-                gameInstance.getWebSocketManager().sendDirection("up");
-            } else if (event.key === "ArrowDown" || event.key === "s") {
-                gameInstance.getWebSocketManager().sendDirection("down");
-            }
-        });
+//     if (mode === "remote") {
+//         // socket, send game start
+//         document.addEventListener("keydown", (event) => {
+//             window.log.info(`Key pressed: ${event.key}`);
+//             if (event.key === "ArrowUp" || event.key === "w") {
+//                 gameInstance.getWebSocketManager().sendDirection("up");
+//             } else if (event.key === "ArrowDown" || event.key === "s") {
+//                 gameInstance.getWebSocketManager().sendDirection("down");
+//             }
+//         });
 
-        document.addEventListener("keyup", (event) => {
-            window.log.info(`Key released: ${event.key}`);
-            if (["ArrowUp", "ArrowDown", "w", "s"].includes(event.key)) {
-                gameInstance.getWebSocketManager().sendDirection("stop");
-            }
-        });
-    }
+//         document.addEventListener("keyup", (event) => {
+//             window.log.info(`Key released: ${event.key}`);
+//             if (["ArrowUp", "ArrowDown", "w", "s"].includes(event.key)) {
+//                 gameInstance.getWebSocketManager().sendDirection("stop");
+//             }
+//         });
+//     }
 
-    if (mode === "local" || "ai") {
-        const engine = createPongEngine();
-        engine.start();
-        engine.onEvent("wall-collision", () => {});
-    }
+//     if (mode === "local" || "ai") {
+//         const engine = createPongEngine();
+//         engine.start();
+//         engine.onEvent("wall-collision", () => {});
+//     }
 
-    // Destroy and clean up
+//     // Destroy and clean up
 
-    // For game, maybe don't include header/footer?
-    return [header, container, footer];
-};
+//     // For game, maybe don't include header/footer?
+//     return [header, container, footer];
+// };
--- frontend/src/scripts/ui/pages/game/managers/managers.state.ts
+++ frontend/src/scripts/ui/pages/game/managers/managers.state.ts
@@ -1,142 +1,130 @@
-import {
-    Ball,
-    Paddle,
-    PongConfig,
-    PongState,
-    PongStatus,
-    Position3D,
-    Size3D,
-    Vector3D,
-} from "@darrenkuro/pong-core";
-import { GameInstance } from "../game.instance";
-import { SceneSetup } from "../game.scene";
+// import { Ball, Paddle, PongState, Size3D } from "@darrenkuro/pong-core";
+// import { GameInstance } from "../game.instance";
+// import { SceneSetup } from "../game.scene";
 
-// import { IServerGameState } from "../game.types";
-// import { GameInstance } from "../renderer/game.instance";
+// export class GameStateManager {
+//     private state = {
+//         status: "waiting",
+//         scores: [0, 0],
+//         ball: {
+//             pos: { x: 0, y: 0, z: 0 },
+//             vec: { x: 0, y: 0, z: 0 },
+//             r: 0.3,
+//             speed: 0.2,
+//         } as Ball,
+//         paddles: [
+//             {
+//                 pos: { x: -10, y: 0.5, z: 0 },
+//                 size: { width: 0.2, height: 0.3, depth: 5 } as Size3D,
+//                 speed: 0.3,
+//             } as Paddle,
+//             {
+//                 pos: { x: 10, y: 0.5, z: 0 },
+//                 size: { width: 0.2, height: 0.3, depth: 5 } as Size3D,
+//                 speed: 0.3,
+//             } as Paddle,
+//         ],
+//     } as PongState;
+//     private gameId = "";
+//     private index = 0;
+//     private opponentId = "";
 
-export class GameStateManager {
-    private state = {
-        status: "waiting",
-        scores: [0, 0],
-        ball: {
-            pos: { x: 0, y: 0, z: 0 },
-            vec: { x: 0, y: 0, z: 0 },
-            r: 0.3,
-            speed: 0.2,
-        } as Ball,
-        paddles: [
-            {
-                pos: { x: -10, y: 0.5, z: 0 },
-                size: { width: 0.2, height: 0.3, depth: 5 } as Size3D,
-                speed: 0.3,
-            } as Paddle,
-            {
-                pos: { x: 10, y: 0.5, z: 0 },
-                size: { width: 0.2, height: 0.3, depth: 5 } as Size3D,
-                speed: 0.3,
-            } as Paddle,
-        ],
-    } as PongState;
-    private gameId = "";
-    private index = 0;
-    private opponentId = "";
+//     getGameId() {
+//         return this.gameId;
+//     }
 
-    getGameId() {
-        return this.gameId;
-    }
+//     getOpponentId() {
+//         return this.opponentId;
+//     }
 
-    getOpponentId() {
-        return this.opponentId;
-    }
+//     getPlayerIndex() {
+//         return this.index;
+//     }
 
-    getPlayerIndex() {
-        return this.index;
-    }
+//     getState() {
+//         return this.state;
+//     }
 
-    getState() {
-        return this.state;
-    }
+//     async startGame(payload: { gameId: string; opponentId: string; index: number }) {
+//         this.state.status = "ongoing";
+//         this.gameId = payload.gameId;
+//         this.index = payload.index;
+//         this.opponentId = payload.opponentId;
+//         // const instance = await GameInstance.getInstance(
+//         //     document.getElementById("renderCanvas") as HTMLCanvasElement
+//         // );
+//         // instance.startGame(payload);
+//     }
 
-    async startGame(payload: { gameId: string; opponentId: string; index: number }) {
-        this.state.status = "ongoing";
-        this.gameId = payload.gameId;
-        this.index = payload.index;
-        this.opponentId = payload.opponentId;
-        // const instance = await GameInstance.getInstance(
-        //     document.getElementById("renderCanvas") as HTMLCanvasElement
-        // );
-        // instance.startGame(payload);
-    }
+//     // TODO: Winner name needs to be fetched from backend routes!
+//     async endGame(winner: string) {
+//         this.state.status = "ended";
+//         const instance = await GameInstance.getInstance(
+//             document.getElementById("renderCanvas") as HTMLCanvasElement
+//         );
+//         SceneSetup.showGameOver(instance.babylon, winner);
+//     }
 
-    // TODO: Winner name needs to be fetched from backend routes!
-    async endGame(winner: string) {
-        this.state.status = "ended";
-        const instance = await GameInstance.getInstance(
-            document.getElementById("renderCanvas") as HTMLCanvasElement
-        );
-        SceneSetup.showGameOver(instance.babylon, winner);
-    }
+//     async updateScore(score: [number, number]) {
+//         if (score[0] !== this.state.scores[0] || score[1] !== this.state.scores[1]) {
+//             this.state.scores = score;
+//             const instance = await GameInstance.getInstance(
+//                 document.getElementById("renderCanvas") as HTMLCanvasElement
+//             );
+//             instance.updateScore(score);
+//         }
+//     }
 
-    async updateScore(score: [number, number]) {
-        if (score[0] !== this.state.scores[0] || score[1] !== this.state.scores[1]) {
-            this.state.scores = score;
-            const instance = await GameInstance.getInstance(
-                document.getElementById("renderCanvas") as HTMLCanvasElement
-            );
-            instance.updateScore(score);
-        }
-    }
+//     async handleWallCollision() {
+//         const instance = await GameInstance.getInstance(
+//             document.getElementById("renderCanvas") as HTMLCanvasElement
+//         );
+//         instance.handleWallCollision();
+//     }
 
-    async handleWallCollision() {
-        const instance = await GameInstance.getInstance(
-            document.getElementById("renderCanvas") as HTMLCanvasElement
-        );
-        instance.handleWallCollision();
-    }
+//     async handlePaddleCollision() {
+//         const instance = await GameInstance.getInstance(
+//             document.getElementById("renderCanvas") as HTMLCanvasElement
+//         );
+//         instance.handlePaddleCollision();
+//         SceneSetup.shakeCamera(instance.babylon);
+//     }
 
-    async handlePaddleCollision() {
-        const instance = await GameInstance.getInstance(
-            document.getElementById("renderCanvas") as HTMLCanvasElement
-        );
-        instance.handlePaddleCollision();
-        SceneSetup.shakeCamera(instance.babylon);
-    }
+//     async resetBall() {
+//         const instance = await GameInstance.getInstance(
+//             document.getElementById("renderCanvas") as HTMLCanvasElement
+//         );
+//         this.state.ball.pos = { x: 0, y: 0, z: 0 };
+//         this.state.ball.vec = { x: 0, y: 0, z: 0 };
+//         instance.handleBallReset();
+//     }
 
-    async resetBall() {
-        const instance = await GameInstance.getInstance(
-            document.getElementById("renderCanvas") as HTMLCanvasElement
-        );
-        this.state.ball.pos = { x: 0, y: 0, z: 0 };
-        this.state.ball.vec = { x: 0, y: 0, z: 0 };
-        instance.handleBallReset();
-    }
+//     async updateGameObjects(payload: PongState) {
+//         const instance = await GameInstance.getInstance(
+//             document.getElementById("renderCanvas") as HTMLCanvasElement
+//         );
+//         if (!payload) return;
 
-    async updateGameObjects(payload: PongState) {
-        const instance = await GameInstance.getInstance(
-            document.getElementById("renderCanvas") as HTMLCanvasElement
-        );
-        if (!payload) return;
+//         instance.updateBall(this.state.ball, payload.ball);
 
-        instance.updateBall(this.state.ball, payload.ball);
+//         instance.updateLeftPaddle(
+//             payload.paddles[0].pos.x,
+//             payload.paddles[0].pos.y,
+//             payload.paddles[0].pos.z
+//         );
 
-        instance.updateLeftPaddle(
-            payload.paddles[0].pos.x,
-            payload.paddles[0].pos.y,
-            payload.paddles[0].pos.z
-        );
+//         instance.updateRightPaddle(
+//             payload.paddles[1].pos.x,
+//             payload.paddles[1].pos.y,
+//             payload.paddles[1].pos.z
+//         );
 
-        instance.updateRightPaddle(
-            payload.paddles[1].pos.x,
-            payload.paddles[1].pos.y,
-            payload.paddles[1].pos.z
-        );
-
-        if (
-            payload.scores[0] !== this.state.scores[0] ||
-            payload.scores[1] !== this.state.scores[1]
-        ) {
-            instance.updateScore(payload.scores);
-        }
-        this.state = payload;
-    }
-}
+//         if (
+//             payload.scores[0] !== this.state.scores[0] ||
+//             payload.scores[1] !== this.state.scores[1]
+//         ) {
+//             instance.updateScore(payload.scores);
+//         }
+//         this.state = payload;
+//     }
+// }
