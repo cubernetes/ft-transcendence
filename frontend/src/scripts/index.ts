6349a1325150fefffdb78275c60f30a04996c797
darrenkuro <odon5ht@gmail.com>
2025-04-09 16:21:05 +0200

    Fix comment style for consistency on index

--- frontend/src/scripts/index.ts
+++ frontend/src/scripts/index.ts
@@ -2,12 +2,12 @@ import earcut from "earcut";
 import config from "./global/config";
 import { logger } from "./utils/logger";
 
-/** Register globally accessible modules and utils */
-window.cfg = config;
-window.log = logger;
-window.earcut = earcut; // Needed for babylonJS?
+// Register globally accessible modules and utils
+window.earcut = earcut; // Needed for babylonJS
+window.cfg = config; // Constants
+window.log = logger; // Register logger
 
-/** Register WebSocket for live reload */
+// Register WebSocket for live reload
 if (process.env.WATCH === "1") {
     const ws = new WebSocket("ws://localhost:35729");
     window.log.info("WebSocket for live reload connected");
@@ -25,7 +25,7 @@ import("./global/router").then(({ createRouter }) => {
         if (appElement) {
             createRouter(appElement);
         } else {
-            window.log.error("Fail to create router, couldn't find id #app");
+            window.log.error("Fail to create router, couldn't find HTMLElement #app");
         }
     };
     document.addEventListener("DOMContentLoaded", launchSite);
