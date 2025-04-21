f07a70b73aa0ba87cbb429bbd94aba12cd474c5e
OwnerOfJK <john.kaller@hotmail.de>
2025-04-21 19:57:47 +0200

    Create tournament store

---
+++ frontend/src/scripts/modules/tournament/tournament.store.ts
@@ -0,0 +1,25 @@
+import { createStore } from "../../global/store";
+
+type TournamentState = {
+    round: "Quarter" | "Semi" | "Final" | null;
+    matches: string[][] | null;
+    current_match: string[] | null;
+    players: string[] | null;
+    winner: string | null;
+};
+
+export const defaultTournamentState: TournamentState = {
+    round: null,
+    matches: null,
+    current_match: null,
+    players: null,
+    winner: null,
+};
+
+export const tournamentStore = createStore<TournamentState>(defaultTournamentState);
+
+tournamentStore.subscribe((state) => {
+    window.log.debug("GameStore subscriber triggered");
+
+    const { round, matches, current_match, players, winner } = state;
+});
