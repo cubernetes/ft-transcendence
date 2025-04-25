97a629a6d271348bea9f6cb6fe4042f29ad82641
OwnerOfJK <john.kaller@hotmail.de>
2025-04-25 14:15:34 +0200

    Create tournament controller

---
+++ frontend/src/scripts/modules/tournament/tournament.controller.ts
@@ -0,0 +1,88 @@
+import { createTournamentTree, updateTournamentTree } from "../../ui/layout/TournamentBracket";
+import { showPageElements } from "../layout/layout.service";
+import { tournamentStore } from "./tournament.store";
+import { MatchState, TournamentState } from "./tournament.types";
+import { determineRound, generateRoundMatches } from "./tournament.utils";
+
+export const createTournamentController = (allPlayers: string[], tree: HTMLElement) => {
+    let unsubscribeTreeUpdate: () => void;
+
+    const createNextRound = () => {
+        const { activePlayers, matches } = tournamentStore.get();
+
+        if (!activePlayers || activePlayers.length <= 1) {
+            window.log.error("Not enough players available for the next round.");
+            return;
+        }
+
+        const roundMatches = generateRoundMatches(activePlayers);
+        const round = determineRound(roundMatches);
+
+        // Safely create a new matches array
+        const newMatches = [...(matches ?? []), roundMatches];
+
+        const partialUpdate: Partial<TournamentState> = {
+            round,
+            matches: newMatches,
+        };
+
+        tournamentStore.update(partialUpdate);
+        window.log.debug("Next Round created: ", tournamentStore.get());
+    };
+
+    const subscribeTournamentTree = () => {
+        unsubscribeTreeUpdate = tournamentStore.subscribe(() => {
+            updateTournamentTree(tree);
+        });
+    };
+
+    const cleanup = () => {
+        if (unsubscribeTreeUpdate) {
+            unsubscribeTreeUpdate();
+        }
+    };
+
+    const startTournament = () => {
+        tournamentStore.set({
+            round: "Default",
+            matches: [],
+            current_match: null,
+            activePlayers: allPlayers,
+            controller,
+        });
+
+        subscribeTournamentTree();
+        createNextRound();
+        updateTournamentTree(tree);
+        window.log.debug("Tournament started");
+    };
+
+    const stopTournament = () => {
+        resetTournament();
+        cleanup();
+    };
+
+    const resetTournament = () => {
+        tournamentStore.set({
+            round: "Default",
+            matches: [],
+            current_match: null,
+            activePlayers: [],
+            controller: null,
+        });
+    };
+
+    const getTournamentTree = () => {
+        return tree;
+    };
+
+    const controller = {
+        createNextRound,
+        startTournament,
+        stopTournament,
+        resetTournament,
+        getTournamentTree,
+    };
+
+    return controller;
+};
