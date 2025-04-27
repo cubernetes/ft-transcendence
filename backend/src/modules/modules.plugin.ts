13a963294ecad38505a342df525e3a20ec80d487
darrenkuro <odon5ht@gmail.com>
2025-04-27 03:26:43 +0200

    Remove tournament module on the backend

--- backend/src/modules/modules.plugin.ts
+++ backend/src/modules/modules.plugin.ts
@@ -3,13 +3,11 @@ import fp from "fastify-plugin";
 // import friendPlugin from "./friend/friend.plugin";
 import gamePlugin from "./game/game.plugin.ts";
 import totpPlugin from "./totp/totp.plugin.ts";
-//import tournamentPlugin from "./tournament/tournament.plugin.ts";
 import userPlugin from "./user/user.plugin.ts";
 
 const modulesPlugin = async (app: FastifyInstance) => {
     // await app.register(friendPlugin);
     await app.register(gamePlugin);
-    //await app.register(tournamentPlugin);
     await app.register(userPlugin);
     await app.register(totpPlugin);
 };
