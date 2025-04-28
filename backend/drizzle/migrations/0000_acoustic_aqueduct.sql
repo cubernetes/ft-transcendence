7dc17c0e547b5bb368a0d4f6d208062fd581f678
darrenkuro <odon5ht@gmail.com>
2025-04-28 02:21:02 +0200

    Generate new dababase migration files (old one stopped working)

--- backend/drizzle/migrations/0000_acoustic_aqueduct.sql
+++ backend/drizzle/migrations/0000_acoustic_aqueduct.sql
@@ -11,37 +11,26 @@ CREATE TABLE `friends` (
 );
 --> statement-breakpoint
 CREATE TABLE `games` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`tournament_id` integer,
+	`id` text PRIMARY KEY NOT NULL,
 	`player1_id` integer NOT NULL,
 	`player2_id` integer NOT NULL,
-	`winner_id` integer,
+	`winner_id` integer NOT NULL,
+	`player1_hits` integer NOT NULL,
+	`player2_hits` integer NOT NULL,
 	`player1_score` integer NOT NULL,
 	`player2_score` integer NOT NULL,
-	`created_at` numeric DEFAULT (CURRENT_TIMESTAMP),
-	`finished_at` numeric,
-	FOREIGN KEY (`tournament_id`) REFERENCES `tournaments`(`id`) ON UPDATE no action ON DELETE no action,
+	`created_at` numeric NOT NULL,
+	`finished_at` numeric DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
 	FOREIGN KEY (`player1_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`player2_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`winner_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
-	CONSTRAINT "games_check_1" CHECK(winner_id IS NULL OR (winner_id = player1_id OR winner_id = player2_id)),
+	CONSTRAINT "games_check_1" CHECK(winner_id = player1_id OR winner_id = player2_id),
 	CONSTRAINT "games_check_2" CHECK(player1_id != player2_id),
 	CONSTRAINT "games_check_3" CHECK(player1_score >= 0 AND player2_score >= 0),
-	CONSTRAINT "game_check_4" CHECK((winner_id IS NULL AND finished_at IS NULL) OR (winner_id IS NOT NULL AND finished_at IS NOT NULL))
+	CONSTRAINT "games_check_4" CHECK(player1_hits >= 0 AND player2_hits >= 0)
 );
 --> statement-breakpoint
-CREATE TABLE `tournaments` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
-	`creator_id` integer NOT NULL,
-	`winner_id` integer,
-	`created_at` numeric DEFAULT (CURRENT_TIMESTAMP),
-	`finished_at` numeric,
-	FOREIGN KEY (`creator_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`winner_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
-	CONSTRAINT "tournaments_check_1" CHECK((winner_id IS NULL AND finished_at IS NULL) OR (winner_id IS NOT NULL AND finished_at IS NOT NULL))
-);
---> statement-breakpoint
+CREATE UNIQUE INDEX `games_id_unique` ON `games` (`id`);--> statement-breakpoint
 CREATE TABLE `users` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`username` text NOT NULL,
@@ -49,11 +38,11 @@ CREATE TABLE `users` (
 	`password_hash` text NOT NULL,
 	`totp_secret` text,
 	`temporary_totp_secret` text,
-	`totp_enabled` integer DEFAULT 0,
-	`avatar_url` text DEFAULT '/assets/default-avatar.png',
-	`wins` integer DEFAULT 0,
-	`losses` integer DEFAULT 0,
-	`created_at` numeric DEFAULT (CURRENT_TIMESTAMP)
+	`totp_enabled` integer DEFAULT 0 NOT NULL,
+	`avatar_url` text DEFAULT '/assets/default-avatar.png' NOT NULL,
+	`wins` integer DEFAULT 0 NOT NULL,
+	`losses` integer DEFAULT 0 NOT NULL,
+	`created_at` numeric DEFAULT (CURRENT_TIMESTAMP) NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);
