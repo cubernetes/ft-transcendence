f84eff09319844600920208013b4c621aad85d03
darrenkuro <odon5ht@gmail.com>
2025-04-28 05:45:04 +0200

    Move swagger plugin out of core

--- backend/src/modules/modules.plugin.ts
+++ backend/src/modules/modules.plugin.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
 // import friendPlugin from "./friend/friend.plugin";
 import gamePlugin from "./game/game.plugin.ts";
+import swaggerPlugin from "./swagger/swagger.plugin.ts";
 import totpPlugin from "./totp/totp.plugin.ts";
 import userPlugin from "./user/user.plugin.ts";
 
@@ -10,6 +11,11 @@ const modulesPlugin = async (app: FastifyInstance) => {
     await app.register(gamePlugin);
     await app.register(userPlugin);
     await app.register(totpPlugin);
+
+    // Register swagger and swagger-ui if not in production
+    if (process.env.NODE_ENV !== "production") {
+        await app.register(swaggerPlugin);
+    }
 };
 
 export default fp(modulesPlugin, { name: "modules-plugin" });
