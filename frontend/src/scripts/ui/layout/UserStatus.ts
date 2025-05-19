3bf22d64e594a6fef4f7f40d1a94c916676e8d7c
darrenkuro <odon5ht@gmail.com>
2025-05-19 13:42:16 +0200

    Clean up styles for user status

--- frontend/src/scripts/ui/layout/UserStatus.ts
+++ frontend/src/scripts/ui/layout/UserStatus.ts
@@ -1,34 +1,29 @@
 import { logout } from "../../modules/auth/auth.service";
-import { getText } from "../../modules/locale/locale.utils";
-import { appendChildren } from "../../utils/dom-helper";
+import { createEl } from "../../utils/dom-helper";
+import { createButton } from "../components/Button";
+import { createContainer } from "../components/Container";
 import { createLanguageButton } from "./LanguageButton";
 
 export const appendUserStatus = (container: HTMLElement, username: string) => {
-    // Create status wrapper
-    const statusWrapper = document.createElement("div");
-    statusWrapper.className =
-        "absolute top-4 right-2 flex items-center justify-between bg-gray-100 rounded p-2 shadow-sm";
-
-    const langBtn = createLanguageButton("static");
     // TODO: Fetch profile pic and display
     // TODO: make name clickable to profile page
 
     // User display name
-    const userNameEl = document.createElement("span");
-    userNameEl.textContent = username;
-    userNameEl.className = "text-gray-700 text-sm";
+    const usernameEl = createEl("span", "text-gray-700 text-sm", { text: username });
+
+    const langBtn = createLanguageButton("static");
 
     // Logout button
-    const logoutBtn = document.createElement("button");
-    logoutBtn.textContent = getText(CONST.TEXT.LOGOUT);
-    logoutBtn.setAttribute(CONST.ATTR.I18N_TEXT, CONST.TEXT.LOGOUT);
-    logoutBtn.className =
-        "bg-red-500 hover:bg-red-600 text-white text-sm py-1 px-3 rounded focus:outline-none";
+    const logoutBtn = createButton({
+        text: CONST.TEXT.LOGOUT,
+        tw: "bg-red-500 hover:bg-red-600 text-white text-sm py-1 px-3 rounded focus:outline-none",
+        click: logout,
+    });
 
-    logoutBtn.onclick = logout;
+    const statusCtn = createContainer({
+        tw: "absolute top-4 right-2 flex items-center justify-between bg-gray-100 rounded p-2 shadow-sm",
+        children: [usernameEl, langBtn, logoutBtn],
+    });
 
-    // Append elements to the wrapper
-    appendChildren(statusWrapper, [userNameEl, langBtn, logoutBtn]);
-
-    container.append(statusWrapper);
+    container.append(statusCtn);
 };
