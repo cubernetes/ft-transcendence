d21cd00979443b38327d36884ef85d056a2484ba
OwnerOfJK <john.kaller@hotmail.de>
2025-05-20 15:02:48 +0200

    Add needed translations for tournament

--- frontend/src/scripts/modules/locale/locale.translation.ts
+++ frontend/src/scripts/modules/locale/locale.translation.ts
@@ -75,6 +75,12 @@ export const TEXT_MAP = {
         fr: "User avatar", // TRANSLATION_NEEDED
         es: "User avatar", // TRANSLATION_NEEDED
     },
+    GAMES_PLAYED: {
+        en: "Played Games",
+        de: "Gespielte Spiele",
+        fr: "Played Games",
+        es: "Played Games",
+    }, // TRANSLATION_NEEDED
     RANK: { en: "Rank", de: "Rang", fr: "Rang", es: "Rango" },
     WINS: { en: "Wins", de: "Siege", fr: "Victoires", es: "Victorias" },
     UPDATE: {
@@ -196,7 +202,66 @@ export const TEXT_MAP = {
         fr: "Nombre de joueurs", // TODO: Check if still correct
         es: "Número de jugadores", // TODO: Check if stil correct
     },
-
+    TOURNAMENT_GAME_COMPLETED: {
+        en: "Completed",
+        de: "Turnier starten", // TRANSLATION_NEEDED
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
+    },
+    TOURNAMENT_GAME_PENDING: {
+        en: "Pending",
+        de: "Turnier starten", // TRANSLATION_NEEDED
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
+    },
+    WINNER: {
+        en: "Winner",
+        de: "Gewinner",
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
+    },
+    START: {
+        en: "Start",
+        de: "Starten",
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
+    },
+    TOURNAMENT_BRACKET: {
+        en: "Tournament Bracket",
+        de: "Turnierbaum", // TRANSLATION_NEEDED
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
+    },
+    WALLET_CONNECT: {
+        en: "🔌 Store Results on the Blockchain",
+        de: "Turnierbaum", // TRANSLATION_NEEDED
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
+    },
+    GET_TOURNAMENT: {
+        en: "📖 Get Tournament History",
+        de: "Turnierbaum", // TRANSLATION_NEEDED
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
+    },
+    RECORD_TOURNAMENT: {
+        en: "📝 Record Game",
+        de: "Turnierbaum", // TRANSLATION_NEEDED
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
+    },
+    RESTART_TOURNAMENT: {
+        en: "🔁 Start Another Tournament",
+        de: "Turnierbaum", // TRANSLATION_NEEDED
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
+    },
+    CHART_PERFROMANCE: {
+        en: "Pong Game Performance",
+        de: "Turnierbaum", // TRANSLATION_NEEDED
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
+    },
     // Server error codes
     USER_NOT_FOUND: {
         en: "User not found",
@@ -325,6 +390,18 @@ export const TEXT_MAP = {
         de: "Wähle eine Schwierigkeit.",
         fr: "Veuillez sélectionner une difficulté.",
         es: "Por favor, seleccione una dificultad.",
+    },
+    NO_TOURNAMENT: {
+        en: "No tournament started.",
+        de: "Kein Tournament wurde gestarted.",
+        fr: "No tournament started.", // TRANSLATION_NEEDED
+        es: "No tournament started.", // TRANSLATION_NEEDED
+    },
+    WALLET_CONNECT_ERROR: {
+        en: "❌ Wallet not detected. Please install a wallet like MetaMask.",
+        de: "Turnierbaum", // TRANSLATION_NEEDED
+        fr: "Démarrer le tournoi", // TRANSLATION_NEEDED
+        es: "Iniciar torneo", // TRANSLATION_NEEDED
     },
 } as const satisfies Record<string, Record<LanguageOpts, string>>;
 
