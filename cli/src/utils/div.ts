34350463841184400b33293ef96fcdadcb91c8a3
bejasch <bschneid@student.42berlin.de>
2025-05-20 18:19:19 +0200

    Add funtion to write lobby updates

--- cli/src/utils/div.ts
+++ cli/src/utils/div.ts
@@ -1,21 +1,43 @@
 import chalk from "chalk";
+import readline from "readline";
 import { PongConfig } from "@darrenkuro/pong-core";
 
+// Function to print lobby info / updates from line 14
 export function showLobbyUpdate(payload) {
     const config: PongConfig = payload.config ? payload.config : payload;
     const playerNames: string[] = payload.playerNames ? payload.playerNames : [];
 
-    console.log(chalk.blue("Lobby Update:"));
-    console.log(chalk.magenta("Configuration:"));
-    console.log(`  - Board: ${config.board.size.width}x${config.board.size.depth}`);
-    console.log(`  - Win Points: ${config.playTo}`);
-    console.log(chalk.magenta("Players:"));
-    if (playerNames.length > 0) {
-        playerNames.forEach((player, index) => {
-            console.log(`  ${index + 1}. ${player}`);
-        });
+    let line = 14;
+
+    readline.cursorTo(process.stdout, 0, line++);
+    readline.clearLine(process.stdout, 0);
+    process.stdout.write(chalk.blue(" Game Configuration:") + "\n");
+
+    readline.cursorTo(process.stdout, 0, line++);
+    readline.clearLine(process.stdout, 0);
+    process.stdout.write(`    - Board: ${config.board.size.width}x${config.board.size.depth}\n`);
+
+    readline.cursorTo(process.stdout, 0, line++);
+    readline.clearLine(process.stdout, 0);
+    process.stdout.write(`    - Win Points: ${config.playTo}\n`);
+
+    readline.cursorTo(process.stdout, 0, line++);
+    readline.clearLine(process.stdout, 0);
+    process.stdout.write(chalk.magenta("  Players:") + "\n");
+
+    readline.cursorTo(process.stdout, 0, line++);
+    readline.clearLine(process.stdout, 0);
+    process.stdout.write(`    1. ${playerNames[0] ? playerNames[0] : chalk.gray("(empty)")}\n`);
+
+    readline.cursorTo(process.stdout, 0, line++);
+    readline.clearLine(process.stdout, 0);
+    if (playerNames[1]) {
+        process.stdout.write(`    2. ${playerNames[1]}\n`);
     } else {
-        console.log("  (no players)");
+        process.stdout.write(
+            `    2. ` + chalk.hex("#FFA500").italic("... waiting for other player ...") + "\n"
+        );
     }
-    console.log("\n");
+
+    // readline.cursorTo(process.stdout, 0, 27);
 }
