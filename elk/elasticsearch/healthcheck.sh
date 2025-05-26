078297e069d1be7c33c41a4b8ca4e3af4bc08112
cubernetes <timo42@proton.me>
2025-05-26 02:25:13 +0200

    Extract healthchecks to separate files

---
+++ elk/elasticsearch/healthcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+: "${ELASTIC_USER:?Missing ELASTIC_USER}"
+: "${ELASTIC_PASSWORD:?Missing ELASTIC_PASSWORD}"
+: "${ELASTICSEARCH_PORT:?Missing ELASTICSEARCH_PORT}"
+
+TERM=linux exec setsid -f watch -xtcn5 curl \
+		--no-progress-meter \
+		--insecure \
+		--user "${ELASTIC_USER}:${ELASTIC_PASSWORD}" \
+		--fail \
+		--write-out '%output{/tmp/healthcheck}%{exitcode}' \
+		"https://localhost:${ELASTICSEARCH_PORT}/_cluster/health" 1>/dev/null 2>&1
