8fd079d51bb1a90b0f13cf8b36d6e3fdeaff5646
OwnerOfJK <john.kaller@hotmail.de>
2025-05-26 20:43:19 +0200

    Remove paddles in setup page

--- frontend/src/scripts/ui/pages/SetupPage.ts
+++ frontend/src/scripts/ui/pages/SetupPage.ts
@@ -1,12 +1,10 @@
 import { layoutStore } from "../../modules/layout/layout.store";
 import { createArcadeWrapper } from "../components/ArcadeWrapper";
 import { createContainer } from "../components/Container";
-import { createPaddles } from "../layout/Paddles";
 import { switchModePanel } from "../layout/SetupPanel";
 
 export const createSetupPage = (): UIComponent => {
     const { router } = layoutStore.get();
-    //const paddles = createPaddles(router);
     const setupCtn = createContainer({
         tw: `w-full p-8 items-center relative ${CONST.STYLES.CONTAINER}`,
         id: CONST.ID.SETUP_CTN, // Not using?
